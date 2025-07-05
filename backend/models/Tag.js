3f7fe7ee38beda6c1ec013d23c1770749aff3979
sayedmahamudsijan <112859381+sayedmahamudsijan@users.noreply.github.com>
2025-07-05 17:14:22 +0600

    Update Tag.js

--- backend/models/Tag.js
+++ backend/models/Tag.js
@@ -1,24 +1,49 @@
+'use strict';
+
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
-  const Tag = sequelize.define('Tag', {
-    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-    name: { type: DataTypes.STRING, unique: true, allowNull: false },
+  class Tag extends Model {
+    static associate(models) {
+      Tag.belongsToMany(models.Template, { 
+        through: models.TemplateTag, // Use TemplateTag model instead of table name
+        foreignKey: 'tag_id', 
+        otherKey: 'template_id', 
+        as: 'Templates',
+        onDelete: 'CASCADE' 
+      });
+    }
+  }
+
+  Tag.init({
+    id: { 
+      type: DataTypes.INTEGER, 
+      autoIncrement: true, 
+      primaryKey: true 
+    },
+    name: { 
+      type: DataTypes.STRING, 
+      unique: true, 
+      allowNull: false 
+    },
+    created_at: { 
+      type: DataTypes.DATE, 
+      allowNull: false 
+    },
+    updated_at: { 
+      type: DataTypes.DATE, 
+      allowNull: false 
+    },
   }, {
+    sequelize,
+    modelName: 'Tag',
+    tableName: 'Tags',
     timestamps: true,
     underscored: true,
     indexes: [
       { fields: ['name'], unique: true },
     ],
   });
-
-  Tag.associate = (models) => {
-    Tag.belongsToMany(models.Template, { 
-      through: 'template_tags', 
-      foreignKey: 'tag_id', 
-      otherKey: 'template_id', 
-      as: 'Templates',
-      onDelete: 'CASCADE' 
-    });
-  };
 
   return Tag;
 };
