3805c6d456d0b9eb1d3acd1d6532ff4f99725ff9
sayedmahamudsijan <112859381+sayedmahamudsijan@users.noreply.github.com>
2025-07-15 23:09:21 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -140,6 +140,7 @@ app.use('/api/comments', require('./routes/commentRoutes'));
 app.use('/api/topics', require('./routes/topicRoutes'));
 app.use('/api/tags', require('./routes/tagRoutes'));
 app.use('/api/likes', require('./routes/likeRoutes'));
+app.use('/api', require('./routes/integrationsRoutes')); // Added integrations routes
 
 // 404 Handler
 app.use((req, res, next) => {
